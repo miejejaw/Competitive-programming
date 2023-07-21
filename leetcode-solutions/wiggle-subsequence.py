f2b56ba36ef27239a6e00018426a1ea55eb3244c
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-07-21 21:54:14 +0300

    Added solution for Wiggle Subsequence

--- leetcode-solutions/wiggle-subsequence.py
+++ leetcode-solutions/wiggle-subsequence.py
@@ -3,18 +3,16 @@ class Solution:
 
         self.memo = {}
         self.length = len(nums)
-        res = self.helper(nums,0,True,nums[0])+1
-        res = max(res,self.helper(nums,0,False,nums[0])+1)
-
+        res = self.helper(nums,0,True)+1
+        res = max(res,self.helper(nums,0,False)+1)
         return res
 
-    def helper(self, nums, ind, isUp, prev):
+    def helper(self, nums, ind, isUp):
         if ind < self.length:       
             if (ind,isUp) not in self.memo:
-                res = self.helper(nums,ind+1,isUp,nums[ind])
-                if (isUp and nums[ind]>prev) or (not isUp and nums[ind]<prev):
-                        res = max(res,self.helper(nums,ind+1,not isUp,nums[ind])+1)
+                res = self.helper(nums,ind+1,isUp)
+                if ind and ((isUp and nums[ind]>nums[ind-1]) or (not isUp and nums[ind]<nums[ind-1])):
+                        res = max(res,self.helper(nums,ind+1,not isUp)+1)
                 self.memo[(ind,isUp)] = res
             return self.memo[(ind,isUp)]
         return 0
-        
