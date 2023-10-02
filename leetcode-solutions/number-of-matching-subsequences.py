f4aa9b5490f3407f2bb2bdd67d872ec60a9f0777
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-10-02 15:04:49 +0300

    Added solution for Number of Matching Subsequences

---
+++ leetcode-solutions/number-of-matching-subsequences.py
@@ -0,0 +1,45 @@
+class TrieNode:
+    def __init__(self):
+        self.children = dict()
+        self.isEnd = False
+
+class Trie:
+    def __init__(self, word):
+        self.root = TrieNode()
+        self.dic = defaultdict(list)
+        length = len(word)
+        for i in range(length):
+            self.dic[word[i]].append(i)
+
+    def insert(self, word):
+        node = self.root
+        for chr in word:
+            if chr not in node.children:
+                node.children[chr] = TrieNode()
+            node = node.children[chr]
+        node.isEnd = True
+
+    def search(self, word):
+        node = self.root
+        idx = -1
+        for chr in word:
+            tmp = bisect_left(self.dic[chr], idx)
+            if tmp == len(self.dic[chr]) or (idx > self.dic[chr][tmp]):
+                return False
+            idx = self.dic[chr][tmp] + 1
+            node = node.children[chr]
+        return True
+              
+
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        trie = Trie(s)
+        for word in words:
+            trie.insert(word)
+
+        count = 0
+        for word in words:
+            if trie.search(word):
+                count += 1
+        
+        return count
