a9c7850436b0b197512231139b31e650398f1e66
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-10-28 15:33:07 +0300

    Added solution for Maximum Population Year

--- leetcode-solutions/maximum-population-year.cpp
+++ leetcode-solutions/maximum-population-year.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
         
-        map<int,int> years;
+        int years[101] = {0};
         for(auto year : logs){
-            years[year[0]] += 1;
-            years[year[1]] -= 1;
+            years[year[0]-1950] += 1;
+            years[year[1]-1950] -= 1;
         }
 
-        int year=0,total=0,max_popu=-1;
-        for (auto it = years.begin(); it != years.end(); ++it) {
-            total += it->second;
+        int year=0,total=0,max_popu=0;
+        for (int i=0; i<100; i++) {
+            total += years[i];
             if (total > max_popu) {
                 max_popu = total;
-                year = it->first;
+                year = i;
             }
         }
-        return year;
+        return year+1950;
     }
 };
