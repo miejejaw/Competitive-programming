1b16b2bd3bb406865f0e3ed7525ec4040d8ed411
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-11-18 18:03:56 +0300

    Added solution for Asteroid Collision

--- leetcode-solutions/asteroid-collision.cpp
+++ leetcode-solutions/asteroid-collision.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int> st;
+        vector<int> res;
         for(int num : asteroids){
             if(num < 0){
-                while(!st.empty() && abs(num) > st.top() && st.top() > 0){
-                    st.pop();
+                while(!res.empty() && -num > res.back() && res.back() > 0){
+                    res.pop_back();
                 }
 
-                if(!st.empty() && abs(num) == st.top())
-                    st.pop();
-                else if(st.empty() || st.top()<0)
-                    st.push(num);
+                if(!res.empty() && -num == res.back())
+                    res.pop_back();
+                else if(res.empty() || res.back()<0)
+                    res.push_back(num);
             }
             else{
-                st.push(num);
+                res.push_back(num);
             }
-        }
-
-        vector<int> res(st.size());
-        for(int i = st.size()-1; i >= 0; i--) {
-            res[i] = st.top();
-            st.pop();
         }
 
         return res;
