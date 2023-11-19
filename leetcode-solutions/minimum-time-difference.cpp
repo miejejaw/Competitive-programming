ae809930b24d7ba3ebc6875af15229702982a3a3
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-11-19 21:16:46 +0300

    Added solution for Minimum Time Difference

--- leetcode-solutions/minimum-time-difference.cpp
+++ leetcode-solutions/minimum-time-difference.cpp
@@ -17,15 +17,13 @@ public:
 
         sort(minutes.begin(),minutes.end());
         int length = minutes.size();
-        int minMin = minutes[0] , maxMin = 0, ans = 1440;
+        int ans = 1440;
 
         for(int i=1; i<length; i++){
             ans = min(ans,minutes[i]-minutes[i-1]);
-            minMin = min(minMin, minutes[i]);
-            maxMin = max(maxMin, minutes[i]);
         }
 
-        ans = min(ans, 1440 - maxMin + minMin);
+        ans = min(ans, 1440 + minutes[0] - minutes[length-1]);
         return ans;
     }
 };
